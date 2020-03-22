0de88aece5f2012d23d26897b822e2b6b65f8cf7
Jesús Calleja Rodríguez <jesus.calleja@talentomobile.com>
2020-03-22 21:51:29 +0100

    Add advices view

---
+++ PandemicTracker/AdvicesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecommendationsViewController.swift
+//  PandemicTracker
+//
+//  Created by Jesús Calleja Rodríguez on 22/03/2020.
+//  Copyright © 2020 Jesús Calleja Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class AdvicesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
